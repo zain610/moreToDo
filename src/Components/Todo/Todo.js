148934592bfaffacfd19dee91252c38ef4715ede
Zain Shroff - zain610 <zainshroff@zains-air-2.home>
2020-05-28 07:21:05 +1000

    Revert "merging rebase branch"
    This reverts commit 8fae68823e6e9a5018daf606231d0fce21850061.

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Todo.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,64 +6,61 @@ import {
   faAngleUp,
   faTrash
 } from "@fortawesome/free-solid-svg-icons";
+import { connect } from "react-redux";
+import { deleteTodo } from "../../actions/actions";
 
-export default class Todo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hide: true
-    };
-    console.log(this.props);
-
-    this.toggleStatus = this.toggleStatus.bind(this);
-  }
-  toggleStatus = () => {
-    this.setState(state => ({
-      hide: !state.hide
-    }));
+function Todo({ listId, todo, dispatch, index }) {
+  const [hide, toggleHide] = useState(true);
+  const toggleStatus = () => {
+    toggleHide(!hide);
   };
-  render() {
-    const iconDirection =
-      this.state.hide === true ? (
-        <FontAwesomeIcon
-          onClick={this.toggleStatus}
-          style={{
-            fontSize: "2rem",
-            padding: "10px 10px 0 10px",
-            color: "mediumpurple"
-          }}
-          icon={faAngleDown}
-        />
-      ) : (
-        <FontAwesomeIcon
-          onClick={this.toggleStatus}
-          style={{
-            fontSize: "2rem",
-            padding: "10px 10px 0 10px",
-            color: "mediumpurple"
-          }}
-          icon={faAngleUp}
-        />
-      );
-    return (
-      <div className="todo">
-        <div className="header">
-          <h1>{this.props.todo.title}</h1>
-          <div className="action-btn-div">
-            {iconDirection}
-            <FontAwesomeIcon
-              onClick={this.props.removeTodo.bind(this, this.props.todo.id)}
-              icon={faTrash}
-              style={{
-                fontSize: "1.5rem",
-                padding: "10px 10px 0 10px",
-                color: "#333"
-              }}
-            />
-          </div>
-        </div>
-        {this.state.hide === false ? <p>{this.props.todo.body}</p> : ""}
-      </div>
+
+  const iconDirection =
+    hide === true ? (
+      <FontAwesomeIcon
+        onClick={toggleStatus}
+        style={{
+          fontSize: "2rem",
+          padding: "10px 10px 0 10px",
+          color: "mediumpurple"
+        }}
+        icon={faAngleDown}
+      />
+    ) : (
+      <FontAwesomeIcon
+        onClick={toggleStatus}
+        style={{
+          fontSize: "2rem",
+          padding: "10px 10px 0 10px",
+          color: "mediumpurple"
+        }}
+        icon={faAngleUp}
+      />
     );
-  }
+  const { id } = todo;
+  return (
+    <div className="todo">
+      <div className="header">
+        <h1>{todo.title}</h1>
+        <div className="action-btn-div">
+          {iconDirection}
+          <FontAwesomeIcon
+            onClick={() => {
+              console.log(listId, index);
+              dispatch(deleteTodo({ listId, id }));
+            }}
+            icon={faTrash}
+            style={{
+              fontSize: "1.5rem",
+              padding: "10px 10px 0 10px",
+              color: "#333"
+            }}
+          />
+        </div>
+      </div>
+      {hide === false ? <p>{todo.body}</p> : ""}
+    </div>
+  );
 }
+
+export default connect()(Todo);
