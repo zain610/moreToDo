51ad208751b451455bee50497195d66a90317d3c
Zain Shroff - zain610 <zainshroff@zains-air-2.home>
2020-06-20 09:19:25 +1000

    WIP: ADD LIST

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TodoList from "./Components/TodoList/TodoList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,20 +7,32 @@ import { connect } from "react-redux";
 import * as actions from "./actions/actions";
 
 function App({ todos, dispatch, lists }) {
+  const [X, setX] = useState(0);
+  const [Y, setY] = useState(0);
+
   const onAddListHandler = () => {
     console.log("handling add list button click");
+    // TODO complete this action
     dispatch(actions.addList({}));
   };
+  const onHandleListDrag = e => {
+    console.log("dragging", e.clientX, e.clientY);
+    setX(e.clientX);
+    setY(e.clientY);
+  };
   return (
-    <div className="App">
+    <div className="App" onDrag={onHandleListDrag}>
       {lists.map((todoList, index) => {
         const { id, header, todos, inputValue } = todoList;
+
         return (
           <TodoList
             inputValue={inputValue}
             listId={index}
             key={id}
             todo={todos}
+            X={X}
+            Y={Y}
           />
         );
       })}
@@ -28,9 +40,12 @@ function App({ todos, dispatch, lists }) {
         class="addTodoList-btn"
         style={{
           display: "float",
-          background: "white",
+          background: "beige",
           height: "max-content",
-          borderRadius: "50px"
+          borderRadius: "50px",
+          position: "absolute",
+          bottom: "1%",
+          right: "1%"
         }}
       >
         <FontAwesomeIcon
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,25 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "../Todo/Todo";
 import Header from "../Todo/Header";
 import { connect } from "react-redux";
 import "../../App.css";
 
-function TodoList({ listId, todo, inputValue }) {
-  //ProTip: Use arrow functions over regular functions to bind _this. Else you need to manually bind this in the constructor
-  // const removeTodo = id => {
-  //   console.log("id", id);
-  //   this.setState(state => ({
-  //     todos: todos.filter(todo => todo.id !== id)
-  //   }));
-  // };
-
-  //Clicks on list border - mousedown
-  //get and record X and Y
+function TodoList({ listId, todo, inputValue, X = 0 }) {
+  let draggingStyle = {
+    position: "absolute",
+    transform: `transform(${X}px)`
+  };
   const onMouseDownHandler = e => {
     console.log(e.clientX, e.clientY);
   };
-  //Leaves border - mouse up
-  //
   const filteredTodos = todo.filter(item => {
     return (
       item.title.toLowerCase().includes(inputValue) ||
@@ -27,7 +19,12 @@ function TodoList({ listId, todo, inputValue }) {
     );
   });
   return (
-    <div className="todoList" onMouseDown={onMouseDownHandler.bind(this)}>
+    <div
+      draggable
+      style={draggingStyle}
+      className="todoList"
+      onClick={onMouseDownHandler}
+    >
       <Header listId={listId} inputValue={inputValue} />
       <hr />
       {filteredTodos.map((todo, i) => (
